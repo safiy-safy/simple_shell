818ec6f70beba4f75079e1366f21e7d6546c4d90
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-11-15 02:20:06 -0800

    simple_shell

---
+++ shell/getppid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t parent_pid;
+
+    parent_pid = getppid();
+    printf("%u\n", parent_pid);
+    return (0);
+}
+
+
---
+++ shell/strtok.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *str = "This is a separated word by spaces";
+        char *stc = malloc(sizeof(char) * strlen(str));
+	char delim[5] = " ";
+        char *trunks;
+	int i;
+
+	strcpy(stc, str);
+        trunks = strtok(stc, delim); 
+        /*printf("%p\n %p\n", str, trunks);*/
+
+        while (trunks != NULL)
+	{
+		printf("%s\n", trunks);
+		trunks = strtok(NULL, delim);
+	}
+
+	for (i = 0; i < 35; i++)
+	{
+		if (stc[i] == '\0')
+			printf("\\0");
+		else
+                printf("%c", stc[i]);
+	}
+          
+        return (0);	
+}
